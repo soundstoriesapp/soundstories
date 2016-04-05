4e16b167d65b9d99b5ebf6a99c4c9f85d6216284
perehn <per.ehnbom@gmail.com>
2016-04-05 12:43:37 +0200

    css

---
+++ grunt-requirejs-build.js
@@ -0,0 +1,25 @@
+module.exports = {
+  appDir: 'public/app-source',
+  baseUrl: '.',
+  mainConfigFile: 'public/app-source/app.js',
+  dir: 'public/dist',
+  modules: [
+    // First set up the common build layer.
+    {
+      // module names are relative to baseUrl
+      name: 'app',
+      // List common dependencies here. Only need to list
+      // top level dependencies, 'include' will find
+      // nested dependencies.
+      include: [
+       
+      ],
+      exclude: [
+           "framework7"
+      ]
+    }
+    
+    
+    
+  ]
+};
